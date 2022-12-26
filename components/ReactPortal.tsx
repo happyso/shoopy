55ba2f9692d87536bebff0bf44c6fa5796b9ecd4
SoYoung Kim <sohappy1220@gmail.com>
2022-12-26 15:19:18 +0900

    feat: add modal

---
+++ components/ReactPortal.tsx
@@ -0,0 +1,46 @@
+import React, { useLayoutEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+function createWrapperAndAppendToBody(wrapperId: string) {
+    const wrapperElement = document.createElement('div')
+    wrapperElement.setAttribute('id', wrapperId)
+    document.body.appendChild(wrapperElement)
+    return wrapperElement
+}
+
+function ReactPortal({
+    children,
+    wrapperId = 'react-portal-wrapper',
+}: {
+    children: React.ReactNode
+    wrapperId: string
+}) {
+    const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(
+        null
+    )
+
+    useLayoutEffect(() => {
+        let element: HTMLElement | null = document.getElementById(wrapperId)
+        let systemCreated = false
+
+        if (!element) {
+            systemCreated = true
+            element = createWrapperAndAppendToBody(wrapperId)
+        }
+        setWrapperElement(element)
+
+        return () => {
+            if (systemCreated && element?.parentNode) {
+                element.parentNode.removeChild(element)
+            }
+        }
+    }, [wrapperId])
+
+    if (wrapperElement === null) return null
+
+    return createPortal(
+        children,
+        document.getElementById(wrapperId) as HTMLElement
+    )
+}
+export default ReactPortal
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import Modal from '../components/Modal'
 
 export default function Home() {
-    return <div className={styles.container}>test</div>
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+        <>
+            <div className={styles.container}>test</div>
+            <button onClick={() => setIsOpen(true)}>Click to Open Modal</button>
+            <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
+                This is Modal Content!
+            </Modal>
+        </>
+    )
 }
